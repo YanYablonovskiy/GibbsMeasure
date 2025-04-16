1c4606dfbe60206507c03528f6003bb4d0efe11e
YanYablonovskiy <yablonovskiy.yan@gmail.com>
2025-04-16 12:34:14 +1000

    Update Proper.lean

--- GibbsMeasure/Prereqs/Kernel/Proper.lean
+++ GibbsMeasure/Prereqs/Kernel/Proper.lean
@@ -61,11 +61,61 @@ lemma IsProper.integral_indicator_mul_indicator (hπ : IsProper π) (h𝓑𝓧 :
 #check IsProper.integral_indicator_mul_indicator
 
 --@indicator X ℝ≥0∞ instENNRealZero (B ∩ A) : (X → ℝ≥0∞) → X → ℝ≥0∞
-
 private lemma IsProper.integral_indicator_mul (hπ : IsProper π) (h𝓑𝓧 : 𝓑 ≤ 𝓧)
     (hf : Integrable[𝓧] f (π x₀)) (hB : MeasurableSet[𝓑] B):
       ∫ x, (B.indicator 1 x) * (f x) ∂(π x₀) = B.indicator 1 x₀ * ∫ x, f x ∂(π x₀) := by
-        sorry
+        apply Integrable.induction (μ:=(π x₀)) (fun (h:X→ℝ) ↦ Integrable[𝓧] h (π x₀) →
+         ∫ x, (B.indicator 1 x) * (h x) ∂(π x₀) = B.indicator 1 x₀ * ∫ x, h x ∂(π x₀))
+        · intro c S hmS bpS _
+          --integral_indicator_mul_indicator hπ h𝓑𝓧 hmS hB
+          --simp_rw [← smul_indicator_one_apply,  smul_eq_mul, ←mul_assoc]
+          --conv_lhs in _ * _ => rw [mul_comm]
+          rw [integral_indicator_const (μ:= (π x₀)) c (hmS)]
+          simp_rw [← smul_indicator_one_apply,  smul_eq_mul, ←mul_assoc]
+          conv_lhs in _ * _ => rw [mul_comm]
+          conv_lhs =>
+           rhs
+           intro x
+           rw [mul_assoc]
+          rw [integral_mul_left]
+          rw [integral_indicator_mul_indicator hπ h𝓑𝓧 hmS hB,←mul_assoc]
+          rw [integral_indicator_one (μ:= (π x₀)) (hmS)]
+          ring
+        · intro f g disj intf intg
+          simp_all
+          intro hp1 hp2
+          conv_lhs =>
+           rhs
+           intro x
+           rw [mul_add]
+          have disj':  Disjoint (Function.support ((B.indicator 1)*f)) (Function.support ((B.indicator 1)*g)) := by
+            simp
+            refine (Disjoint.inter_left' B ?_)
+            refine (Disjoint.inter_right' B ?_)
+            exact disj
+          have intBf: Integrable (μ := (π x₀))  (((B.indicator 1)*f) : X → ℝ ) := by sorry
+          have intBg: Integrable (μ := (π x₀))  (((B.indicator 1)*g) : X → ℝ ) := by sorry
+          rw [integral_add intf intg]
+          have fun_add: ∀x, (B.indicator 1 x * f x + B.indicator 1 x * g x
+           = (((B.indicator 1)*f):X → ℝ) x + (((B.indicator 1)*g):X → ℝ) x ) := by
+            intro x
+            simp
+          simp [fun_add]
+          rw [integral_add intBf intBg]
+          simp
+          rw [hp1,hp2]
+          ring
+        · refine IsComplete.isClosed ?_
+          intro f1 cf1 fleF
+          have := memLp_one_iff_integrable.mpr hf
+          have := this.toLp
+          use this
+          simp
+          sorry
+
+
+
+
 
 
 #check Disjoint.inter_right'
